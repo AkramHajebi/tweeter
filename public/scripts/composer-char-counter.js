71d6b8f717312a655b64cf9387cb4826d6d994b0
AkramHajebi <ahajebi2000@email.com>
2020-12-18 00:10:02 +0000

    Update counter of tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log('heloooooo')
+  //console.log('heloooooo');
+  const newTweet=$('#tweet-text');
+  let counter= $(".counter");
+  console.log(counter.val);
+  
+  newTweet.on('keyup',function() {
+    //console.log(this);
+    //console.log($(this));
+    //console.log('heloooooo');
+    let textLength = $(this).val().length;
+    console.log(textLength);
+    /*if(textLength > 15){
+    alert('Length is exceeded');
+    //Do ur stuff;
+
+  } */
+  let leftChar = 140 - textLength;
+  counter.val(leftChar);
+  if (leftChar < 0) {
+    counter.css('color','red');
+   }
+   else {
+    counter.css('color','black');
+   }
+  
+  });
 });
